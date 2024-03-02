c4766d173790474da328f72615aa68462d01b814
Jaime de Salas <jaimedesalasjuanas@gmail.com>
2024-03-02 18:21:22 +0100

    Added CM

--- aerocoeff.py
+++ aerocoeff.py
@@ -47,7 +47,7 @@ def calculate_coeff(force, density, airspeed, wingarea):
     """
     return 2*force/(wingarea*density*airspeed**2)
 
-def extract_and_coefficients(path, velocity, density, area):
+def extract_and_coefficients(path, velocity, density, area, chord):
     # moment coefficient still to be implemented
     """Extracts for .txt and calculates coefficients
     Args:
@@ -60,31 +60,37 @@ def extract_and_coefficients(path, velocity, density, area):
         o_aoa (np.array): ordered list of angles of attack
         o_cl (np.array): ordered list of lift coefficients
         o_cd (np.array): ordered list of drag coefficients
+        o_cm (np.array): ordered list of moment coefficients
     """
     aoas = []
     liftcoeffs = []
     dragcoeffs = []
+    momentcoeffs = []
 
     df = pd.read_csv(path, sep='\s+')
     for i in range(df.shape[0]):
         global_forces = aircraft2global(np.array([df.loc[i, 'Fx'], 0, df.loc[i, 'Fz']]), df.loc[i, 'AoA']*np.pi/180)
         cl = calculate_coeff(global_forces[2], density, velocity, area)
         cd = calculate_coeff(-global_forces[0], density, velocity, area)
+        cm = calculate_coeff(df.loc[i, 'My'], density, velocity, area*chord)
 
         aoas = np.append(aoas, df.loc[i, 'AoA'])
         liftcoeffs = np.append(liftcoeffs, cl)
         dragcoeffs = np.append(dragcoeffs, cd)
+        momentcoeffs = np.append(momentcoeffs, cm)
 
     order = np.argsort(aoas)
     o_aoa = aoas[order]
     o_cl = liftcoeffs[order]
     o_cd = dragcoeffs[order]
+    o_cm = momentcoeffs[order]
     
-    return o_aoa, o_cl, o_cd
+    return o_aoa, o_cl, o_cd, o_cm
 
 if __name__ == "__main__":
     RHO0 = 1.225
     WING_AREA = 73
+    CHORD = 1.8
 
     CRUISE_H = 22000 * 0.3048 #Convert form feet to meters
     CRUISE_RHO = isa(CRUISE_H)[2]
@@ -96,16 +102,16 @@ if __name__ == "__main__":
 
     data2plot = [None, None, None, None]
     #-----------------CRUISE SBW----------------------
-    data2plot[0] = extract_and_coefficients(r"./Dataset/SBW/CRUISE/Global_forces.txt", CRUISE_V, CRUISE_RHO, WING_AREA)
+    data2plot[0] = extract_and_coefficients(r"./Dataset/SBW/CRUISE/Global_forces.txt", CRUISE_V, CRUISE_RHO, WING_AREA, CHORD)
 
     #-----------------CLIMB SBW----------------------
-    data2plot[1] = extract_and_coefficients(r"./Dataset/SBW/CLIMB/Global_forces.txt", CLIMB_V, CLIMB_RHO, WING_AREA)
+    data2plot[1] = extract_and_coefficients(r"./Dataset/SBW/CLIMB/Global_forces.txt", CLIMB_V, CLIMB_RHO, WING_AREA, CHORD)
 
     #-----------------CRUISE CNT----------------------
-    data2plot[2] = extract_and_coefficients(r"./Dataset/CNT/CRUISE/Global_forces.txt", CRUISE_V, CRUISE_RHO, WING_AREA)
+    data2plot[2] = extract_and_coefficients(r"./Dataset/CNT/CRUISE/Global_forces.txt", CRUISE_V, CRUISE_RHO, WING_AREA, CHORD)
 
     #-----------------CLIMB CNT----------------------
-    data2plot[3] = extract_and_coefficients(r"./Dataset/CNT/CLIMB/Global_forces.txt", CLIMB_V, CLIMB_RHO, WING_AREA)
+    data2plot[3] = extract_and_coefficients(r"./Dataset/CNT/CLIMB/Global_forces.txt", CLIMB_V, CLIMB_RHO, WING_AREA, CHORD)
 
 
     for i in range(4):
@@ -117,7 +123,14 @@ if __name__ == "__main__":
 
     for i in range(4):
         plt.plot(data2plot[i][2], data2plot[i][1], label=f"{i}", marker='.') # Drag Buckets
-    
+
     plt.legend()
     plt.title("Drag Polars")
+    plt.show()
+
+    for i in range(4):
+        plt.plot(data2plot[i][0], data2plot[i][3], label=f"{i}", marker='.') # CM-alpha
+
+    plt.legend()
+    plt.title("CM-alpha")
     plt.show()
