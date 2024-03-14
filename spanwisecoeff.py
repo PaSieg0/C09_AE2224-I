386e2e064f92d4da3c12a165e27886333a8342b3
Jaime de Salas <jaimedesalasjuanas@gmail.com>
2024-03-14 14:28:32 +0100

    Added low fidelity plots

--- spanwisecoeff.py
+++ spanwisecoeff.py
@@ -5,9 +5,13 @@ from isa_calc import isa
 
 df_CNT = pd.read_csv(r"./Dataset/CNT/CRUISE/Surface_loadings.txt", sep='\s+') # Load the data CNT
 df_SBW = pd.read_csv(r"./Dataset/SBW/CRUISE/Surface_loadings.txt", sep='\s+') # Load the data SBW
+df_low_fidelity_wing = pd.read_csv(r"./Dataset/low_fidelity/CRUISE/Wing_surface_laodings.dat", sep='\s+') # Load the data low fidelity wing
+df_low_fidelity_strut = pd.read_csv(r"./Dataset/low_fidelity/CRUISE/Strut_surface_laodings.dat", sep='\s+') # Load the data low fidelity strut
 
 plt.plot(df_CNT.iloc[:, 0], df_CNT.iloc[:, 2], label='CNT')
 plt.plot(df_SBW.iloc[:, 0], df_SBW.iloc[:, 1], label='SBW')
+plt.plot(df_low_fidelity_wing.iloc[:, 0], df_low_fidelity_wing.iloc[:, 2], label='Low fidelity wing')
+plt.plot(df_low_fidelity_strut.iloc[:, 0], df_low_fidelity_strut.iloc[:, 2], label='Low fidelity strut')
 plt.xlabel('y')
 plt.ylabel('Cl')
 plt.title('Cl-y')
@@ -17,6 +21,8 @@ plt.clf()
 
 plt.plot(df_CNT.iloc[:, 0], df_CNT.iloc[:, 3], label='CNT')
 plt.plot(df_SBW.iloc[:, 0], df_SBW.iloc[:, 2], label='SBW')
+plt.plot(df_low_fidelity_wing.iloc[:, 0], df_low_fidelity_wing.iloc[:, 3], label='Low fidelity wing')
+plt.plot(df_low_fidelity_strut.iloc[:, 0], df_low_fidelity_strut.iloc[:, 3], label='Low fidelity strut')
 plt.xlabel('y')
 plt.ylabel('Cd')
 plt.title('Cd-y')
@@ -26,6 +32,8 @@ plt.clf()
 
 plt.plot(df_CNT.iloc[:, 0], df_CNT.iloc[:, 2]/df_CNT.iloc[:, 3], label='CNT')
 plt.plot(df_SBW.iloc[:, 0], df_SBW.iloc[:, 1]/df_SBW.iloc[:, 2], label='SBW')
+plt.plot(df_low_fidelity_wing.iloc[:, 0], df_low_fidelity_wing.iloc[:, 2]/df_low_fidelity_wing.iloc[:, 3], label='Low fidelity wing')
+plt.plot(df_low_fidelity_strut.iloc[:, 0], df_low_fidelity_strut.iloc[:, 2]/df_low_fidelity_strut.iloc[:, 3], label='Low fidelity strut')
 plt.xlabel('y')
 plt.ylabel('Cl/Cd')
 plt.title('ClCd-y')
@@ -35,6 +43,8 @@ plt.clf()
 
 plt.plot(df_CNT.iloc[:, 0], df_CNT.iloc[:, 1], label='CNT')
 plt.plot(df_SBW.iloc[:, 0], df_SBW.iloc[:, 3]/df_SBW.iloc[:, 1], label='SBW')
+plt.plot(df_low_fidelity_wing.iloc[:, 0], df_low_fidelity_wing.iloc[:, 1], label='Low fidelity wing')
+plt.plot(df_low_fidelity_strut.iloc[:, 0], df_low_fidelity_strut.iloc[:, 1], label='Low fidelity strut')
 plt.xlabel('y')
 plt.ylabel('chord')
 plt.title('chord-y')
